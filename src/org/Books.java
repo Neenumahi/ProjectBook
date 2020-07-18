a79f0a6dc3992e94eff23a5ecf053b13169703d4
Neenumahi <neenumahi2104@gmail.com>
2020-07-18 16:03:00 -0700

    msg

---
+++ src/org/Books.java
@@ -0,0 +1,15 @@
+package org;
+
+public class Books {
+	
+		public void firstLang() {
+			System.out.println("First language is Tamil");
+		}
+		
+	public void secondLang() {
+		System.out.println("second language is English");
+	}
+	}
+
+
+
